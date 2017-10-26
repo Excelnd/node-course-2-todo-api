e08f1ce34270ca6313c2c038026318391a397b4d
ihsan <ihsanmpm@gmail.com>
2017-10-26 07:38:03 +0530

    setup app for mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect('mongodb://ihsanmpm:accounter123@ds233895.mlab.com:33895/dbjabal' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
