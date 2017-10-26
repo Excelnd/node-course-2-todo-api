d02ec0cfbea9a0ca39a180c3031e201e5c414cbc
ihsan <ihsanmpm@gmail.com>
2017-10-26 09:06:10 +0530

    Setup aa for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-mongoose.connect('mongodb://ihsanmpm:accounter123@ds233895.mlab.com:33895/dbjabal' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
